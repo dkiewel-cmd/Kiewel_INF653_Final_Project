19e7ef8ee797676b8a19af74f8ef26568d281c49
dkiewel-cmd <d_kiewel@mail.fhsu.edu>
2026-05-06 07:15:45 +1000

    root change 2

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use('/', express.static(path.join(__dirname, '/public')));
 
 // routes
-app.use('/', require('./routes/root'));
+app.use(/^\/$|^\/index(\.html)?$/, require('./routes/root'));
 app.use('/states', require('./routes/api/states'));
 
 // catch-all
