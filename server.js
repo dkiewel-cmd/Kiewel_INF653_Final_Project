f3f43ca22969d8396cf183106e7893f15448d2d5
dkiewel-cmd <d_kiewel@mail.fhsu.edu>
2026-05-06 08:08:55 +1000

    0.0.5

--- server.js
+++ server.js
@@ -20,13 +20,13 @@ app.use(logger);
 app.use(cors(corsOptions));
 
 // built-in middleware to handle urlencoded form data
-app.use(express.urlencoded({ extended: true }));
+//app.use(express.urlencoded({ extended: true }));
 
 // built-in middleware for json
-app.use(express.json());
+//app.use(express.json());
 
 // serve static files
-app.use('/', express.static(path.join(__dirname, '/public')));
+//app.use('/', express.static(path.join(__dirname, '/public')));
 
 // routes
 app.use('/', require('./routes/root'));
