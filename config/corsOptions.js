e84095a8d406c83f4447873a91f5b1fda30b2813
dkiewel-cmd <d_kiewel@mail.fhsu.edu>
2026-05-08 16:10:19 +1000

    0.0.19

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -6,13 +6,8 @@ const whitelist = [
 ];
 
 const corsOptions = {
-    origin: (origin, callback) => {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: '*',
+    methods: "GET,PUT,PATCH,POST,DELETE",
     optionsSuccessStatus: 200
 }
 
