b42e9a415234a3be6827a8a272fae50287148f00
dkiewel-cmd <d_kiewel@mail.fhsu.edu>
2026-05-09 11:37:23 +1000

    0.0.39

--- model/State.js
+++ model/State.js
@@ -2,10 +2,62 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const stateSchema = new Schema({
-    stateCode: {
-        type: String,
-        required: true,
-        unique: true
+    state: {
+        type: String
+    },
+    slug: {
+        type: String
+    },
+    code: {
+        type: String
+    },
+    nickname: {
+        type: String
+    },
+    website: {
+        type: String
+    },
+    admission_date: {
+        type: String
+    },
+    admission_number: {
+        type: Number
+    },
+    capital_city: {
+        type: String
+    },
+    capital_url: {
+        type: String
+    },
+    population: {
+        type: Number
+    },
+    population_rank: {
+        type: Number
+    },
+    constitution_url: {
+        type: String
+    },
+    state_flag_url: {
+        type: String
+    },
+    state_seal_url: {
+        type: String
+    },
+    map_image_url: {
+        type: String
+    },
+    landscape_background_url: {
+        type: String
+    },
+    skyline_background_url: {
+        type: String
+    },
+    twitter_url: {
+        type: String
+    },
+    facebook_url: {
+        type: String
     },
     funfacts: {
         type: [String]
